3373b7c0adec64572daf465beeec3e284bf7af47
NATUREMILKTEA <khoinguyen09022001@gmail.com>
2021-10-31 15:50:20 +0700

    xong bàn phím

--- lib/main.dart
+++ lib/main.dart
@@ -102,6 +102,86 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+
+            Row(
+              mainAixsAlignment: MainAxisAlignment.center,
+              children: <Widget>
+              Container(
+                width: MediaQuery.of(Context).size.width *.75,
+                child: Table(
+                  children: [
+                    TableRow(
+                      children: [
+                    buildButton("C",1,Colors.redAccent),
+                    buildButton("⨞",1,Colors.blue),
+                    buildButton("÷",1,Colors.blue),
+                  ]
+                ),
+                TableRow(
+                      children: [
+                    buildButton("7",1,Colors.black54),
+                    buildButton("8",1,Colors.black54),
+                    buildButton("9",1,Colors.black54),
+                  ]
+                ),
+
+                TableRow(
+                      children: [
+                    buildButton("4",1,Colors.black54),
+                    buildButton("5",1,Colors.black54),
+                    buildButton("6",1,Colors.black54),
+                  ]
+                ),
+                
+                 TableRow(
+                      children: [
+                    buildButton("1",1,Colors.black54),
+                    buildButton("2",1,Colors.black54),
+                    buildButton("3",1,Colors.black54),
+                  ]
+                ),
+
+                 TableRow(
+                      children: [
+                    buildButton(".",1,Colors.black54),
+                    buildButton("0",1,Colors.black54),
+                    buildButton("00",1,Colors.black54),
+                  ]
+                ),
+              
+              ),
+
+              Container(
+                width: MediaQuery.of(Context).size.width * 0.25,
+                child: Table(
+                  children: [
+                    TableRow(
+                      children: [
+                        buildButton("×",1,Colors.blue),
+                      ]
+                    ),
+                    TableRow(
+                      children: [
+                        buildButton("⁻",1,Colors.blue),
+                      ]
+                    ),
+                    TableRow(
+                      children: [
+                        buildButton("⁺",1,Colors.blue),
+                      ]
+                    ),
+                    TableRow(
+                      children: [
+                        buildButton("=",1,Colors.redAccent),
+                      ]
+                    ),
+                  ]
+                )
+              )
+
+            ),
+
+          
           ],
         ),
       ),
